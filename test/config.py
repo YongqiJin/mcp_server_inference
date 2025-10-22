555841e595c60af76412d4c8e1bc5adf2c00a870
YongqiJin <yongqijin0112@gmail.com>
2025-10-22 07:59:24 +0000

    bohr svg

--- test/config.py
+++ test/config.py
@@ -1,7 +1,7 @@
 config = {
   "mcpServers": {
     "nmr": {
-      "url": "https://0c2f66f43752d47fb49abeea0badf47a.app-space.dplink.cc/sse?token=b5964458cbe44fc38e0305d2f7bf3acf"
+      "url": "https://nmr-uuid1751618147.app-space.dplink.cc/sse?token=a72cceea446d4dfbacd03e0023270946"
     }
   }
 }
